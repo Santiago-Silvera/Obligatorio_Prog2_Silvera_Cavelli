f72cf88b27d52e049bdde66c79a21f6126999a3d
santi34mg <ssilvera@correo.um.edu.uy>
2024-05-25 01:38:21 -0300

    Algo mas prolijo

--- node-server/src/script.js
+++ node-server/src/script.js
@@ -1,29 +1,6 @@
-document.getElementById("btn1").addEventListener("click", () => {
-  fetch("/api/button1")
+function handleButton(buttonNumber) {
+  fetch(`/api/button${buttonNumber}`)
     .then((response) => response.text())
-    .then((data) => console.log(data));
-});
-
-document.getElementById("btn2").addEventListener("click", () => {
-  fetch("/api/button2")
-    .then((response) => response.text())
-    .then((data) => console.log(data));
-});
-
-document.getElementById("btn3").addEventListener("click", () => {
-  fetch("/api/button3")
-    .then((response) => response.text())
-    .then((data) => console.log(data));
-});
-
-document.getElementById("btn4").addEventListener("click", () => {
-  fetch("/api/button4")
-    .then((response) => response.text())
-    .then((data) => console.log(data));
-});
-
-document.getElementById("btn5").addEventListener("click", () => {
-  fetch("/api/button5")
-    .then((response) => response.text())
-    .then((data) => console.log(data));
-});
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+}
