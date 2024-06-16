5c007fc8411be7b471258d9a78a8a469f538e7cb
2512i <2512ignacio@gmail.com>
2024-06-16 12:33:21 -0300

    <Clase Song modificada, todos los atributos agregados, hashCode: id_song (atributos con mismo nombre que CSV)>

--- backend/src/Sistema/System.java
+++ backend/src/Sistema/System.java
@@ -8,7 +8,7 @@ public class System {
     public static MyHash<DateCountryPair, MyList<Song>> topSongsByDateCountry = new MyHashTable<>(50, 0.75f);
 
     public static void main(String[] args) {
-        CSVLoader loader = new CSVLoader("C:\\Users\\santi\\IdeaProjects\\obligatorio\\universal_top_spotify_songs.csv");
+        CSVLoader loader = new CSVLoader("");
         loader.LoadCSV();
     }
 }
